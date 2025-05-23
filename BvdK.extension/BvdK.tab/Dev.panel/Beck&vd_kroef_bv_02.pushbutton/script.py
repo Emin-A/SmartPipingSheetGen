08e56ec485c282fa21ef44dd45439edf153ebacd
Emin-A <eminavdovic@gmail.com>
2025-05-23 18:05:14 +0200

    Setting up the Check Reducers script logic before proceeding to create the script.

---
+++ BvdK.extension/BvdK.tab/Dev.panel/Beck&vd_kroef_bv_02.pushbutton/script.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+__title__ = "Check\nReducers"
+__doc__ = """Version = 1.0
+Date    = 23.05.2025
+________________________________________________________________
+Description:
+
+This integrated script lets you:
+  1. Fix reducer orientations and parameters automatically.
+  2. Enforce family/type rulse based on pipe diameter threshold.
+  3. Replace long fittings with short variants.
+  4. Control eccentricity, length options, and elbow configurations dynamically
+________________________________________________________________
+How-To:
+
+1. [Hold ALT + CLICK] on the button to open its source folder.
+2. Create your boundary (with detail lines) in the view.
+3. Click the button and follow the prompts.
+________________________________________________________________
+Author: Emin Avdovic"""
